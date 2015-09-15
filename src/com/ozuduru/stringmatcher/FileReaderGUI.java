044bf5bf448d0ee21f796881d54ff306acb8c8e3
onurozuduru <onur.ozuduru@gmail.com>
2015-09-15 19:52:36 +0300

    License is updated.

--- src/com/ozuduru/stringmatcher/FileReaderGUI.java
+++ src/com/ozuduru/stringmatcher/FileReaderGUI.java
@@ -5,10 +5,14 @@
 *   github: github.com/onurozuduru
 *   twitter: twitter.com/OnurOzuduru
 *
-*License: This work is licensed under the
-*   Creative Commons Attribution-ShareAlike 4.0 International License.
-*   To view a copy of this license,
-*   visit http://creativecommons.org/licenses/by-sa/4.0/.
+*License: MIT
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*   SOFTWARE.
 */
 
 package com.ozuduru.stringmatcher;
